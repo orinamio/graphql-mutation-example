126bd605ca9cc0de9b4e9591f5b7f61e495f16e7
Orinami Olatunji <orinamiolatunji@gmail.com>
2017-06-24 21:58:43 +0100

    update server files

---
+++ data/quote.js
@@ -0,0 +1,9 @@
+const Quotes = [
+  { id: "454dccf0-9ebc-457b-b9d1-313255a7ddea", quote: "When Chuck Norris was born he drove his mom home from the hospital." },
+  { id: "ac2b5539-b036-4aeb-9e62-ace5a95942ab", quote: "Chuck Norris has a diary. It's called the Guinness Book of World Records." },
+  { id: "ab648889-f53a-4fdd-9e22-511bcfe45dcc", quote: "Chuck Norris threw a grenade and killed 50 people, then it exploded." },
+  { id: "a6cf34b9-c317-450b-bfc0-346599df21a1", quote: "Chuck Norris counted to infinity. Twice." },
+  { id: "5a1c7249-c3f4-4321-8b44-4f49707a1c9c", quote: "Chuck Norris beat the sun in a staring contest." }
+];
+
+module.exports = Quotes;
--- src/schema.js
+++ src/schema.js
@@ -1,57 +1,51 @@
-const { GraphQLID, GraphQLInt, GraphQLFloat, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLNonNull,
-  GraphQLObjectType, GraphQLInputObjectType, GraphQLEnumType, GraphQLSchema } = require('graphql');
-
-const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://chucknorris:admin@ds135522.mlab.com:35522/chucknorris');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Database Connected!")
-});
-
-const Schema = mongoose.Schema;
-
-const quoteSchema = new Schema({
-  _id:  String,
-  query: String
-});
-
-const Quote = mongoose.model('Quotes', quoteSchema);
-
+const uuidv4 = require('uuid/v4');
+const _ = require("lodash");
+const { GraphQLID, GraphQLInt, GraphQLFloat, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLInputObjectType, GraphQLEnumType, GraphQLSchema } = require('graphql');
+const Quotes = require('../data/quote.js');
 
 const QuoteType = new GraphQLObjectType({
   name: 'QuoteType',
   description: 'Chuck Norris Quotes',
   fields: {
-    _id: { type: GraphQLString },
+    id: { type: GraphQLString },
     quote: { type: GraphQLString }
   }
-})
+});
+const QuoteCreateType = new GraphQLInputObjectType({
+  name: 'QuoteCreateType',
+  type: QuoteType,
+  fields: {
+    quote: { type: new GraphQLNonNull(GraphQLString) }
+  }
+});
+
+const QuoteUpdateType = new GraphQLInputObjectType({
+  name: 'QuoteUpdateType',
+  type: QuoteType,
+  fields: {
+    id: {type: new GraphQLNonNull(GraphQLString)},
+    quote: { type: new GraphQLNonNull(GraphQLString) }
+  }
+});
+
+const QuoteDeleteType = new GraphQLInputObjectType({
+  name: 'QuoteDeleteType',
+  type: QuoteType,
+  fields: {
+    id: {type: new GraphQLNonNull(GraphQLString)}
+  }
+});
 
 const ChuckNorrisQueryType = new GraphQLObjectType({
   name: 'ChuckNorrisQuerytype',
   description: 'Chuck Norris Query Schema',
   fields: {
     quotes: {
-      type: QuoteType,
-      resolve: () => Quote.find((err, quotes) => {
-        if(err){
-          console.error(err);
-        }
-        console.log(quotes);
-        return quotes;
-      })
+      type: new GraphQLList(QuoteType),
+      resolve: () => Quotes 
     }
   }
 });
-
-const QuoteInputType = new GraphQLInputObjectType({
-  name: 'QuoteInputType',
-  fields: {
-    quote: { type: new GraphQLNonNull(GraphQLString) }
-  }
-})
-
 
 const ChuckNorrisMutationType = new GraphQLObjectType({
   name: 'ChuckNorrisMutationType',
@@ -60,36 +54,43 @@ const ChuckNorrisMutationType = new GraphQLObjectType({
     createQuote: {
       type: QuoteType,
       args: {
-        quote: { type: QuoteInputType }
+        quote: { type: new GraphQLNonNull(QuoteCreateType) }
       },
       resolve: (source, { quote }) => {
-        Quote.create((err, quote) => {
-          if (err) {
-            console.error(err);
-          }
-          console.log(quote);
-        })
+        quote.id = uuidv4();
+        quote.quote = quote.quote;
+
+        Quotes.push(quote);
+        return quote;
+      }
+    },
+    updateQuote: {
+      type: QuoteType,
+      args: {
+        quote: { type: new GraphQLNonNull(QuoteUpdateType) }
+      },
+      resolve: (source, {quote}) => {
+        let quoteData = [];
+        quoteData.id = quote.id;
+        quoteData.quote = quote.quote;
+
+        let index = Quotes.indexOf(quote.id);
+        let updateQuote = Quotes.splice(index, 1, quoteData);
+
+        return quote;
+      }
+    },
+    deleteQuote: {
+      type: QuoteType,
+      args: {
+        quote: {type: new GraphQLNonNull(QuoteDeleteType)}
+      },
+      resolve: (source, {quote}) => {
+        let value = _.remove(Quotes, q => q.id == quote.id)
+        return quote;
       }
     }
-    // },
-    // updateQuote: {
-    //   type: QuoteType,
-    //   args: {
-    //     _id: {type: new GraphQLNonNull(GraphQLString)}
-    //   },
-    //   resolve: (source, {_id}) => {
-        
-    //   }
-    // },
-    // deleteQuote: {
-    //   type: QuoteType,
-    //   args: {
-    //     _id: {type: new GraphQLNonNull(GraphQLString)}
-    //   },
-    //   resolve: (source, {_id}) => {
-        
-    //   }
-    }
+  }
 });
 
 const ChockNorrisSchema = new GraphQLSchema({
